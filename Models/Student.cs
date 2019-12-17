706bb46f2d75e7e4a2c63bb5b7d419f50be444ce
an.nh <nguyenhuuan1997@gmail.com>
2019-12-17 22:35:35 +0700

    Create models: Classroom, LateEvent, Student, LateSetting. Create DB Context.

---
+++ Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HappyMVCAssignment.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required()]
+        public string Name { get; set; }
+        [Required()]
+        public string Code { get; set; }
+        [Required()]
+        public string Phone { get; set; }
+        [Required()]
+        public string Email { get; set; }
+        public StudentStatus Status { get; set; }
+        public enum StudentStatus
+        {
+            Active = 1,
+            DeActive = 0,
+            Saved = 2,
+            Deleted = 3
+        }
+
+        public int ClassroomId { get; set; }
+        public virtual Classroom Classroom { get; set; }
+        public virtual ICollection<LateEvent> LateEvents { get; set; }
+    }
+}

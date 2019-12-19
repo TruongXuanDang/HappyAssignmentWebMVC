a3dc82af04e093b3555110814daa85e7f46ac133
TruongDX8 <truonghusk17@gmail.com>
2019-12-19 22:52:09 +0700

    Fix default view

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HappyMVCAssignment
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Classrooms", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

83cb3cdad028e4e3112d228f7814c7bfbdc2d5d7
TruongDX8 <truonghusk17@gmail.com>
2019-12-30 19:45:18 +0700

    Add AccountsController

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,43 @@
+﻿using HappyMVCAssignment.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyMVCAssignment.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private HappyMVCAssignmentContext db = new HappyMVCAssignmentContext();
+        private UserManager<Account> userManager;
+        private RoleManager<AccountRole> roleManager;
+
+        public AccountsController()
+        {
+            UserStore<Account> userStore = new UserStore<Account>(db);
+            userManager = new UserManager<Account>(userStore);
+        }
+
+        // GET: Accounts
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessRegister(string username, string password)
+        {
+            var account = new Account()
+            {
+                UserName = username,
+                FirstName = "Xuan Hung",
+                LastName = "Dao"
+            };
+            IdentityResult result = userManager.Create(account, password);
+            return View("Register");
+        }
+    }
+}

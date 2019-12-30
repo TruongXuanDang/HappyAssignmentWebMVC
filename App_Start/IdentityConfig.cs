fcd76db5c46c4b01fc0ece301fa35aeb26fedd91
TruongDX8 <truonghusk17@gmail.com>
2019-12-30 19:15:22 +0700

    Add IdentityConfig

---
+++ App_Start/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartup(typeof(HappyMVCAssignment.App_Start.IdentityConfig))]
+namespace HappyMVCAssignment.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
+        }
+    }
+}
